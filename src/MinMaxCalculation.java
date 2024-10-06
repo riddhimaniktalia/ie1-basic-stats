3972e9db5fff93a23ef282b3db1afcfb34a71da6
ReshmaAshok <reshmaashok2000@gmail.com>
2024-10-06 16:19:51 -0400

    Modified MinMaxCalculation.java to compute minimum of a set of numbers

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -1,19 +1,26 @@
 //Maximum part is done by Riddhi Maniktalia
-
+//Min is done by Reshma Ashok
 public class MinMaxCalculation {
     public static void main(String[] args) {
         int[] numbers = { 10, 5, 8, 20, 3 };
 
         int max = numbers[0];
+        int min = numbers[0];
 
         for (int i = 1; i < numbers.length; i++) {
             
             if (numbers[i] > max) {
                 max = numbers[i];
             }
+
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
         }
+
 
         
         System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
     }
 }
